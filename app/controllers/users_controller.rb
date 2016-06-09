27975f19c63e1793c6270aa28947553e60a7397c
Pablo Rivero <pablofalons5@gmail.com>
2016-06-09 19:03:30 -0430

    Added dashboard for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :dashboard]
+  before_action :find_document, only: [:show, :dashboard]
+
   def index
     @users = User.all.order("created_at DESC")
   end
 
   def show
-    @user = User.find(params[:id])
+
+  end
+
+  def dashboard
+
+  end
+
+  private
+
+  def find_document
     @documents = Document.where(user_id: @user).order("created_at DESC")
+  end
+
+  def find_user
+    if params[:id].nil?
+        @user= current_user
+    else
+      @user = User.find(params[:id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'welcome#index'
 
   resources :users, only: [:index, :show] do
     resources :documents
   end
 
+  authenticated :user do
+    root 'users#dashboard', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
