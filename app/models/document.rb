32b8326d77b3cc76d17fa98dc578fd5b8a43c135
Pablo Rivero <pablofalons5@gmail.com>
2016-07-04 23:16:53 -0430

    Added validation to _form new document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,8 @@
 class Document < ActiveRecord::Base
   belongs_to :user
+  validates :title, presence: true, length: {minimun: 20}
+  validates :description, presence: true
+  validates :pdf, presence: true
 
   has_attached_file :pdf
   validates_attachment :pdf, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
