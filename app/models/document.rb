304dda5653b73e728b7a77e3cf5ede15245dfdf1
Pablo Rivero <pablofalons5@gmail.com>
2016-07-05 15:28:27 -0430

    custom validations for error handaling forms

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,8 +1,16 @@
 class Document < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true
-  validates :description, presence: true, length: {minimum: 20}
-  validates :pdf, presence: true
+
+  validates :title,
+          :presence => {:message => "El titulo no puede estar en blanco" },
+          :length => { :in => 20..100, :message => "El titulo debe estar entre 20 a 100 caracteres"}
+
+  validates :description,
+          :presence => {:message => "La descripcion no puede estar en blanco" },
+          :length => { :minimum => 20, :message => "La descripcion debe tener mas de 20 caracteres"}
+
+  validates :pdf,
+          :presence => {:message => "No hay ningun archivo para subir" }
 
   has_attached_file :pdf
   validates_attachment :pdf, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
