900c38b58bcd9469a70f580fceb49813b8d1098f
Pablo Rivero <pablofalons5@gmail.com>
2016-07-06 01:47:29 -0430

    Fix some broken links and validation on document.rb

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,7 @@
 class Document < ActiveRecord::Base
   belongs_to :user
-  after_validation :clean_paperclip_errors
+
+   after_validation :clean_paperclip_errors
 
   validates :title,
           :presence => {:message => "El titulo no puede estar en blanco" },
@@ -12,12 +13,11 @@ class Document < ActiveRecord::Base
 
 
 
-
-   def clean_paperclip_errors
-     errors.delete(:pdf)
-   end
-
   has_attached_file :pdf
-  validates_attachment :pdf, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document), :message => 'Solo puedes subir archivos con formato .pdf' },
-                             :presence => {:message => "No hay ningun archivo para subir" }
+  validates_attachment :pdf, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document), :message => 'Solo puedes subir archivos con formato .pdf' }
+
+  private
+  def clean_paperclip_errors
+    errors.delete(:pdf)
+  end
 end
