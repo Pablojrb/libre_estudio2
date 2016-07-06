3aa811feefb0eeca56be7751eed74601ccd1b59f
Pablo Rivero <pablofalons5@gmail.com>
2016-07-05 23:48:50 -0430

    custom validation for paperclip uploading pdf documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,6 @@
 class Document < ActiveRecord::Base
   belongs_to :user
+  after_validation :clean_paperclip_errors
 
   validates :title,
           :presence => {:message => "El titulo no puede estar en blanco" },
@@ -9,9 +10,14 @@ class Document < ActiveRecord::Base
           :presence => {:message => "La descripcion no puede estar en blanco" },
           :length => { :minimum => 20, :message => "La descripcion debe tener mas de 20 caracteres"}
 
-  validates :pdf,
-          :presence => {:message => "No hay ningun archivo para subir" }
+
+
+
+   def clean_paperclip_errors
+     errors.delete(:pdf)
+   end
 
   has_attached_file :pdf
-  validates_attachment :pdf, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+  validates_attachment :pdf, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document), :message => 'Solo puedes subir archivos con formato .pdf' },
+                             :presence => {:message => "No hay ningun archivo para subir" }
 end
