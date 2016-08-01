c64e031e7827b0ede3ad8735b3a47dccca7383f1
Pablo Rivero <pablofalons5@gmail.com>
2016-08-01 17:28:35 -0430

    fix error whit toggleClass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,36 +16,21 @@
 //= require_tree .
 
 (function ($) {
-  // var $mobileNavToggleBtn = $('mobile-nav-t');
-  // function onBtnClick(e) {
-  //   var $selectors = $('mobile-nav-t','mobile-nav');
-  //   $selectors.toggleClass('is-open');
-  // }
+
 
   $(document).ready(function(){
-    // $mobileNavToggleBtn.on('click',onBtnClick)
     setTimeout(function(){
       $('#notice_wrapper').fadeOut("slow", function() {
         $(this).remove();
       })
     },2500);
-    consola();
-    mobileNav();
+    $('.mobile-nav-t').click(function(){
+      $('.mobile-nav').toggleClass('is-open');
+      $('.mobile-nav-t').toggleClass('is-open');
+
+      console.log("funciono");
+    });
 
   });
 
 })(jQuery);
-
-function mobileNav(){
-
-  $('mobile-nav-t').click(function(){
-
-    console.log("clikeado");
-    var status = $('this').hasClass('.is-open');
-    if(status){$('.mobile-nav-t','.mobile-nav').removeClass('.is-open');}
-    else{$('.mobile-nav-t','.mobile-nav').addClass('.is-open');}
-  });
-}
-function consola(){
-  console.log("hola mundo munaskjdhaskjdo");
-}
